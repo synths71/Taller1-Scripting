96ac836f77165db983331fa9bc463b61c0586bb0
synths71 <jagm4126@gmail.com>
2026-01-29 11:50:02 -0500

    prueba

---
+++ EjercicioCiclos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_ejercicios_scripting
+{
+    internal class EjercicioCiclos
+    {
+        int numeroSecreto = 7;
+        int intentosMaximos = 3;
+
+    }
+}
