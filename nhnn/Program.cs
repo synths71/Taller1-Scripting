bccf3a4ef0ba765c81724c53d07b88e27efdb9e5
synths71 <jagm4126@gmail.com>
2026-01-30 00:08:16 -0500

    dsa

---
+++ nhnn/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace nhnn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //dsadsadad
+        }
+    }
+}
---
+++ uy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    //Este es uno de los 2 ejercicio de funciones. 
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        int[] resultado = InvertirArray(nums);
+
+        foreach (int n in resultado)
+            Console.Write(n + " ");
+    }
+
+    static int[] InvertirArray(int[] arr)
+    {
+        int[] invertido = new int[arr.Length];
+        int j = 0;
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            invertido[j] = arr[i];
+            j++;
+        }
+        return invertido;
+    }
+
+
+}
