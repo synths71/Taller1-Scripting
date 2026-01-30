2cff86a682c0c533e4eaa7486c9e20d88d194aaa
synths71 <jagm4126@gmail.com>
2026-01-29 22:03:56 -0500

    ejercicios

--- Program.cs
+++ Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        //Este es uno de los 2 ejercicio de funciones. 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int[] resultado = InvertirArray(nums);
+
+            foreach (int n in resultado)
+                Console.Write(n + " ");
         }
+
+        static int[] InvertirArray(int[] arr)
+        {
+            int[] invertido = new int[arr.Length];
+            int j = 0;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                invertido[j] = arr[i];
+                j++;
+            }
+            return invertido;
+        }
+
     }
 }
